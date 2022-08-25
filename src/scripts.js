f4a47ec70ea015a314423b7b2e1b380814f95494
binhussen <mhdhsna@gmail.com>
2022-08-25 19:50:22 +0300

    Add post method with fetch

--- src/scripts.js
+++ src/scripts.js
@@ -2,22 +2,32 @@
 
 import './styles.css';
 
-const scores = [
-  {
-    name: 'Mohammed',
-    score: 100,
-  },
-  {
-    name: 'Hussen',
-    score: 90,
-  },
-  {
-    name: 'Abdu',
-    score: 90,
-  },
-];
+// const scoresEl = document.querySelector('.scores');
+const nameEl = document.querySelector('.name');
+const scoreEl = document.querySelector('.score');
+const submitEl = document.querySelector('.btn-submit');
+const API_URL =
+	'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ze1TjZFGUQAXVEhzCJJJ/scores/';
 
-const scoresEl = document.querySelector('.scores');
-scores.forEach(({ name, score }) => {
-  scoresEl.innerHTML += `<li class="score">${name} : ${score}</li>`;
-});
+// scores.forEach(({ name, score }) => {
+// 	scoresEl.innerHTML += `<li class="score">${name} : ${score}</li>`;
+// });s
+
+const addScore = async (event) => {
+	event.preventDefault();
+	await postGame({ user: nameEl.value, score: +scoreEl.value });
+	nameEl.value = '';
+	scoreEl.value = '';
+};
+
+const postGame = async (score) => {
+	await fetch(API_URL, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(score),
+	});
+};
+
+submitEl.addEventListener('click', addScore);
